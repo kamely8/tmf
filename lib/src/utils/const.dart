ae43344e8a889e9322265f4b2ba20d9af5c68cb8
kamely8 <100317243+kamely8@users.noreply.github.com>
2022-03-02 17:04:29 -0500

    Delete lib directory

--- lib/src/utils/const.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-final themeColor = Color(0xfff5a623);
-final primaryColor = Color(0xff203152);
-final greyColor = Color(0xffaeaeae);
-final greyColor2 = Color(0xffE8E8E8);
-
-final String pathPrincipal = 'http://www.cartopel.com/ords/ws_web/';
